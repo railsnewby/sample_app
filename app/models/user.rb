bc147d7a1d77f931e38f120223725b2c38289023
railsnewby <mailforrails@gmail.com>
2017-03-21 09:55:08 +0300

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
 	has_secure_password
   before_save { email.downcase! }
-	 validates :name,  presence: true, length: { maximum: 50 }
+	validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-
   validates :password, length: { minimum: 6 }
 
   def User.new_remember_token
@@ -14,6 +14,11 @@ class User < ActiveRecord::Base
 
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  def feed
+    # Это предварительное решение. См. полную реализацию в "Following users".
+    Micropost.where("user_id = ?", id)
   end
 
   private
